02506fc5a1e28b42b78013df781e723a3dd37c92
Chorong Shin <chorongstellashin@gmail.com>
2022-09-27 10:34:17 -0700

    Completed removeSmallest.js

--- algorithm-challenge/removeSmallest.js
+++ algorithm-challenge/removeSmallest.js
@@ -9,7 +9,10 @@ function removeSmallest(numbers) {
 }
 
 /*
-"Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list."
+"Given an array of integers, remove the smallest value.
+Do not mutate the original array/list.
+If there are multiple elements with the same value,
+remove the one with a lower index. If you get an empty array/list, return an empty array/list."
 */
 
 
