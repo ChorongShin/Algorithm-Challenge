79785cabffd1e69aade2fa76c7bbe841c3fd864a
Chorong Shin <chorongstellashin@gmail.com>
2022-10-07 10:16:13 -0700

    Completed calculatingwithFunctions

---
+++ algorithm-challenge/calcuatingWithFunctions.js
@@ -0,0 +1,84 @@
+function zero(func) {
+  if (func) {
+    return func(0)
+  }
+    return 0;
+
+};
+function one(func) {
+   if (func) {
+    return func(1)
+  }
+    return 1
+};
+function two(func) {
+   if (func) {
+    return func(2)
+  }
+    return 2
+};
+function three(func) {
+   if (func) {
+    return func(3)
+  }
+    return 3
+};
+function four(func) {
+   if (func) {
+    return func(4)
+  }
+    return 4
+};
+function five(func) {
+   if (func) {
+    return func(5)
+  }
+    return 5
+};
+function six(func) {
+  if (func) {
+    return func(6)
+  }
+    return 6;
+
+};
+function seven(func) {
+   if (func) {
+    return func(7)
+  }
+    return 7;
+};
+function eight(func) {
+   if (func) {
+    return func(8)
+  }
+  return 8;
+};
+function nine(func) {
+ if (func) {
+    return func(9)
+  }
+  return 9
+};
+
+function plus(b) {
+  return function( a ) {
+    return a + b; };
+};
+function minus(b) {
+  return function( a ) {
+    return a - b; };
+};
+function times( b ) {
+  return function( a ) {
+    return a * b; };
+};
+function dividedBy( b ) {
+  return function( a ) {
+    return Math.floor(a / b); };
+};
+
+
+//Examples
+eight(dividedBy(three())) // 2
+five(times(seven())) //35
