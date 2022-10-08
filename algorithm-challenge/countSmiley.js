078f323f03eb1d193bfa9a25c719686945c93600
Chorong Shin <chorongstellashin@gmail.com>
2022-10-08 16:25:58 -0700

    Completed countSmileys

---
+++ algorithm-challenge/countSmiley.js
@@ -0,0 +1,17 @@
+function countSmileys(arr) {
+  let count = 0;
+
+  const smileys = [":)",";)",":D",";D",":-D",":~D",":-)",":~)",";~D",";~)",";-D",";-)"]
+;
+
+
+  for (let i = 0; i < arr.length; i++) {
+    if (smileys.includes(arr[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+countSmileys([':)', ';(', ';}', ':-D']) // 2
+countSmileys([';->' , ';o>' , ':~(' , ':)' , ':-(' , ':o)' , ';D']) // 2
