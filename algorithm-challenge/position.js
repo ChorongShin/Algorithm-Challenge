8b1ec086b07d35ecdb2b6f855e5c5105546b1c74
Chorong Shin <chorongstellashin@gmail.com>
2022-12-11 21:08:06 +0900

    Completed position of alphabet algorithem

---
+++ algorithm-challenge/position.js
@@ -0,0 +1,8 @@
+function position(letter) {
+  return `Position of alphabet: ${letter.charCodeAt(0) - 96}`
+}
+
+
+//Examples:
+position('a') //--> Position of alphabet: 1
+position('z') //--> Position of alphabet: 26
